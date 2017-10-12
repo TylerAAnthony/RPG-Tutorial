00d33ca7ed8f9a015faecc86749b3bd699601cd7
Tyler Anthony <tyleranthony1906@gmail.com>
2017-10-12 10:20:17 -0500

    Finished adding ability to add and remove items from inventory screen

---
+++ RPG-Project/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+	
+	Inventory inventory;
+
+	public Transform itemsParent;
+
+
+	// Use this for initialization
+	void Start () {
+		inventory = Inventory.instance;
+		inventory.OnItemChangedCallback += UpdateUI;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateUI ()
+	{
+        InventorySlot[] slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+		for (int i = 0; i < slots.Length; i++)
+		{
+			if (i < inventory.items.Count) {
+				slots[i].AddItem(inventory.items[i]);
+			} else 
+			{
+				slots[i].ClearSlot ();
+
+			}
+		}
+	}
+		
+}
